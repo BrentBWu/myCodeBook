1485ea986e59e75c70de029c4e2de32858908a57
吴柏文 <bowen@wubaiwendeMacBook-Pro.local>
2021-07-13 10:28:00 +0800

    680.js 2

--- 680.js
+++ 680.js
@@ -20,52 +20,88 @@
  * @param {string} s
  * @return {boolean}
  */
- var validPalindrome = function(s) {
-    let i = 0, l = s.length - 1;
-    let checkout_i =0, checkout_l =0;
-    let way = 0;
-    while(s[i]===s[l] && i<l) {
+//  var validPalindrome = function(s) {
+//     let i = 0, l = s.length - 1;
+//     let checkout_i =0, checkout_l =0;
+//     let way = 0;
+//     while(s[i]===s[l] && i<l) {
+//         i++;
+//         l--;
+//     }
+//     if(s[i]===s[l]) return true;
+//     if(s[i]===s[l-1]) {
+//         checkout_l = l;
+//         checkout_l--;
+//         checkout_i = i
+//         way = 0;
+//     } else {
+//         checkout_i = i;
+//         checkout_i++;
+//         checkout_l = l;
+//         way = 1;
+//     }
+//     while(s[checkout_i]===s[checkout_l] && checkout_i<checkout_l) {
+//         checkout_i++;
+//         checkout_l--;
+//     }
+//     if(s[checkout_i]===s[checkout_l]) {
+//         console.log('1232');
+//         return true;
+//     }
+//     if(checkout_i === checkout_l) {
+//         console.log('1');
+//         return true;
+//     } else {
+//         way===0?i++:l--;
+//         while(s[i]===s[l] &&i<l) {
+//             i++;
+//             l--;
+//         }
+//         if(s[i]===s[l]) return true;
+//         if(i===l){
+//             console.log('121');
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// };
+var validPalindrome = function(s) {
+    let i = 0, j = s.length - 1;
+    while(s[i]==s[j] && i<j){
         i++;
-        l--;
+        j--;
     }
-    if(s[i]===s[l]) return true;
-    if(s[i]===s[l-1]) {
-        checkout_l = l;
-        checkout_l--;
-        checkout_i = i
-        way = 0;
-    } else {
-        checkout_i = i;
-        checkout_i++;
-        checkout_l = l;
-        way = 1;
-    }
-    while(s[checkout_i]===s[checkout_l] && checkout_i<checkout_l) {
-        checkout_i++;
-        checkout_l--;
-    }
-    if(s[checkout_i]===s[checkout_l]) {
-        console.log('1232');
+    if(s[i]==s[j]) {
+        console.log('111');
         return true;
-    }
-    if(checkout_i === checkout_l) {
-        console.log('1');
+    };
+    if(validWayOut(s,i+1,j)||validWayOut(s,i,j-1)){
+        console.log('222');
         return true;
     } else {
-        way===0?i++:l--;
-        while(s[i]===s[l] &&i<l) {
-            i++;
-            l--;
-        }
-        if(s[i]===s[l]) return true;
-        if(i===l){
-            console.log('121');
-            return true;
-        } else {
-            return false;
-        }
+        return false
     }
-};
+}
 
-let s = "abc"
+var validWayOut = function(s,start, end) {
+    while(s[start]==s[end] && start<end) {
+        start++;
+        end--;
+    }
+    if(s[start]==s[end]){
+        console.log('333');
+        return true;
+    };
+    if(start==end) {
+        console.log('true');
+        return true;
+    } else {
+        console.log('false');
+        return false;
+    }
+}
+
+
+let s = "atbbga"
 console.log(validPalindrome(s));
