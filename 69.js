d354cea5986646cbe25ab03a67c2b74406877ac3
吴柏文 <bowen@lalaland.local>
2021-08-04 15:10:02 +0800

    69.js

--- 69.js
+++ 69.js
@@ -4,8 +4,6 @@
 
 // Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
 
- 
-
 // Example 1:
 
 // Input: x = 4
@@ -15,7 +13,6 @@
 // Input: x = 8
 // Output: 2
 // Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
- 
 
 // Constraints:
 
@@ -24,21 +21,38 @@
  * @param {number} x
  * @return {number}
  */
- var mySqrt = function(x) {
-    if(x==0) return x;
-    let left = 1, right = x, sqrt, mid;
-    while(left <= right) {
-        mid = left + (right - left) / 2; // l + r可能会溢出
-        sqrt = x / mid;
-        if(sqrt == mid) {
-            return mid;
-        } else if (sqrt > mid) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-    return right;
+var mySqrt = function (x) {
+  if(x===0) return x;
+  let left = 1, right = x, sqrt, mid;
+  while(left <= right) {
+      mid = parseInt(left + (right - left) / 2); // l + r可能会溢出
+      sqrt = x / mid;
+      if(sqrt === mid) {
+          return mid;
+      } else if (sqrt > mid) {
+          left = mid + 1;
+      } else {
+          right = mid - 1;
+      }
+  }
+  return parseInt(right);
+//   if (a == 0) return a;
+//   let l = 1,
+//     r = a,
+//     mid,
+//     sqrt;
+//   while (l <= r) {
+//     mid = parseInt(l + (r - l) / 2);
+//     sqrt = a / mid;
+//     if (sqrt == mid) {
+//       return parseInt(mid);
+//     } else if (mid > sqrt) {
+//       r = mid - 1;
+//     } else {
+//       l = mid + 1;
+//     }
+//   }
+//   return parseInt(r);
 };
-const a = 9;
+const a = 4;
 console.log(mySqrt(a));
