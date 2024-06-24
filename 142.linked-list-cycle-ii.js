5ca6e56fa57ebe299264a96dd496985e4bdee095
Wu Bowen <bowen.wu@mobilityasia.com.cn>
2024-06-24 10:48:27 +0800

    update linked-list-cycle-ii

--- 142.linked-list-cycle-ii.js
+++ 142.linked-list-cycle-ii.js
@@ -15,10 +15,29 @@
 
 /**
  * @param {ListNode} head
- * @return {ListNode}
+ * @return {lili}
  */
 var detectCycle = function(head) {
-    
+    if (!head || !head.next) {
+        return null;
+    }
+    let pFast = head.next.next;
+    let pSlow = head.next;
+
+    //Floyd's cycle-finding algorithm
+    while(pFast != pSlow) {
+        if(pFast === null || pFast.next === null) {
+            return null;
+        }
+        pFast = pFast.next.next;
+        pSlow = pSlow.next;
+    }
+    pFast = head;
+    while(pSlow != pFast) {
+        pSlow = pSlow.next;
+        pFast = pFast.next;
+    }
+    return pFast;
 };
 // @lc code=end
 
