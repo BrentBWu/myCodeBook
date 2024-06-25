6ad9a3ba01627de3a2a3207422989fc973e4df65
Wu Bowen <bowen.wu@mobilityasia.com.cn>
2024-06-25 18:20:47 +0800

    524.js

--- 524.longest-word-in-dictionary-through-deleting.js
+++ 524.longest-word-in-dictionary-through-deleting.js
@@ -11,7 +11,22 @@
  * @return {string}
  */
 var findLongestWord = function(s, dictionary) {
-    
+    let output = '';
+    dictionary.sort();
+    for(let i=0; i<dictionary.length; i++) {
+        let j=0;
+        for(let m=0; j<dictionary[i].length && m < s.length;) {
+            if(dictionary[i][j] === s[m]) {
+                j++;
+            }
+            m++;
+        }
+        if(j === dictionary[i].length) {
+                output = output.length >= dictionary[i].length && output ? output : dictionary[i];
+        }
+    }
+    return output;
 };
 // @lc code=end
-
+let s = "abce", dictionary = ["abe","abc"]
+console.log(findLongestWord(s, dictionary),'output')
